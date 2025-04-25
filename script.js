b8e326ae3c1cd1a1a1f99fe323ebb822b121903f
Nicholas Macedo <nicholasmacedoo@gmail.com>
2025-04-25 10:34:34 -0300

    feat: Introdução ao Javascript na web - Conceito de hoisting - Recurso da window do browser (prompt,alert e confirm) - Recurso da DOM utilizando o `document` para manipular a dom.
    Closes #1

---
+++ script.js
@@ -0,0 +1,33 @@
+let nome = "Nicholas"
+
+if(true) {
+    let sobrenome = "Macedo"
+    console.log("Bem vindo Sr." + nome + " " + sobrenome)
+}
+
+console.log("--------------------------------------")
+// console.log(sobrenome)
+
+// let idade = 32
+// console.log("Idade é " + idade)
+
+
+// if(idade >= 18) {
+//     let sobrenome = "Macedo";
+    
+//     // const deAcordo = window.confirm("Você está de acordo com os termos ?")
+
+//     if(deAcordo) {
+//         // alert("Seja bem-vindo Sr." + nome + " " + sobrenome)
+//     }
+//     // console.log(deAcordo)
+// }
+
+
+
+const deAcordo = window.confirm("Você está de acordo com os termos?")
+console.log(deAcordo)
+
+
+document.write("show!!!")
+// document.write("<h1>Minha primeira manipulação</h1>", "<h1>Minha primeira manipulação</h1>")
